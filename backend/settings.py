bb996a1055f62a11b8d8483f06f1ca8058bf0118
Burak CAN <drcan94@gmail.com>
2022-08-18 00:34:29 +0300

    some changes

--- backend/settings.py
+++ backend/settings.py
@@ -165,7 +165,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, "static/")
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
