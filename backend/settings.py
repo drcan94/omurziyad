6d696e8c822edd5c2dd60bfb55d39037726adf8e
Burak CAN <drcan94@gmail.com>
2022-08-18 00:44:55 +0300

    some changes

--- backend/settings.py
+++ backend/settings.py
@@ -153,16 +153,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/")
-]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = "/images/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
