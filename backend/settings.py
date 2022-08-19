afef6a664c4162408a497112cc681db02f222887
Burak CAN <drcan94@gmail.com>
2022-08-19 21:55:54 +0300

    some changes

--- backend/settings.py
+++ backend/settings.py
@@ -4,24 +4,16 @@ import os
 import environ
 
 env = environ.Env()
-
 environ.Env.read_env()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#)3wbj-5h#5pp57g4+5rv8_j)q=f4c7^u#n#qn44l=jtaryso_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "omurziyad.herokuapp.com"]
-
-# Application definition
+ALLOWED_HOSTS = ["127.0.0.1", "omurziyad.heroku.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -104,9 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -121,7 +110,7 @@ DATABASES = {
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 # Password validation
@@ -142,26 +131,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOWED_ORIGINS = [
--- base/admin.py
+++ base/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from base.models import OmurInitials
+
+admin.site.register(OmurInitials)
