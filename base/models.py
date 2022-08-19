10e3f07ad6166591dc03d71b8e57c361293ceb48
Burak CAN <drcan94@gmail.com>
2022-08-20 00:16:25 +0300

    some changes

--- base/models.py
+++ base/models.py
@@ -6,3 +6,6 @@ class OmurInitials(models.Model):
     commenter = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.commenter.username
