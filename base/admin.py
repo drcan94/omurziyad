af5bed2c490eec756b635cfb96af748f9c1c80ba
Burak CAN <drcan94@gmail.com>
2022-08-20 23:35:45 +0300

    some changes

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from base.models import OmurInitials
 
-admin.site.register(OmurInitials)
+
+@admin.register(OmurInitials)
+class OmurInitials(admin.ModelAdmin):
+    list_display = ("id", "commenter", "content", "created_at")
--- base/models.py
+++ base/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 
 class OmurInitials(models.Model):
+
+    class Meta:
+        verbose_name = "İlk"
+        verbose_name_plural = "ilkler"
+
     commenter = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
