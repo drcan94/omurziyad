1744b4841780a57328bf5887ac64cf49045db668
Burak CAN <drcan94@gmail.com>
2022-08-24 22:29:56 +0300

    some changes

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -4,6 +4,4 @@ from base.views import user_views as views
 
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name="login"),
-    path('profile/', views.get_user_profile, name="user-profile"),
-    path('profile/update/', views.update_user_profile, name="user-profile-update"),
 ]
